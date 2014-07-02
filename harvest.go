b038f7ca113397484a2125a18d3ece1b893f7cd8
Michael Wagner <mitch.wagna@gmail.com>
2014-07-02 20:30:55 +0200

    Implement remaining user actions

--- harvest.go
+++ harvest.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/url"
@@ -63,4 +64,91 @@ func (h *HarvestClient) Find(id int) (*User, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (h *HarvestClient) Create(user *User) (*User, error) {
+	marshaledUser, err := json.Marshal(user)
+	if err != nil {
+		return nil, err
+	}
+	response, err := h.client.Post("/people", "application/json", bytes.NewBuffer(marshaledUser))
+	if err != nil {
+		return nil, err
+	}
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (h *HarvestClient) ResetPassword(user *User) error {
+	marshaledUser, err := json.Marshal(user)
+	if err != nil {
+		return err
+	}
+	_, err = h.client.Post("/people/"+string(user.Id)+"/reset_password", "application/json", bytes.NewBuffer(marshaledUser))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *HarvestClient) Update(user *User) (*User, error) {
+	marshaledUser, err := json.Marshal(user)
+	if err != nil {
+		return nil, err
+	}
+	request, err := http.NewRequest("PUT", "/people/"+string(user.Id), bytes.NewBuffer(marshaledUser))
+	if err != nil {
+		return nil, err
+	}
+	request.Header = http.Header{"Content-Type": {"application/json"}}
+	response, err := h.client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (h *HarvestClient) Delete(user *User) (bool, error) {
+	request, err := http.NewRequest("DELETE", "/people/"+string(user.Id), nil)
+	if err != nil {
+		return false, err
+	}
+	response, err := h.client.Do(request)
+	if err != nil {
+		return false, err
+	}
+	if response.StatusCode == 200 {
+		return true, nil
+	} else if response.StatusCode == 400 {
+		return false, nil
+	} else {
+		panic(response.Status)
+	}
+}
+
+func (h *HarvestClient) Toggle(user *User) (bool, error) {
+	request, err := http.NewRequest("DELETE", "/people/"+string(user.Id), nil)
+	if err != nil {
+		return false, err
+	}
+	response, err := h.client.Do(request)
+	if err != nil {
+		return false, err
+	}
+	if response.StatusCode == 200 {
+		return true, nil
+	} else if response.StatusCode == 400 {
+		return false, nil
+	} else {
+		panic(response.Status)
+	}
 }
