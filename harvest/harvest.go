0507e336d870048910f395d816878e6fa920e790
Michael Wagner <mitch.wagna@gmail.com>
2014-12-28 17:26:38 +0100

    Introduce Api struct to perform CRUD requests on API

--- harvest/harvest.go
+++ harvest/harvest.go
@@ -124,3 +124,183 @@ type ResponseError struct {
 func (r *ResponseError) Error() string {
 	return r.ErrorPayload.Message
 }
+
+type NotFound interface {
+	error
+	NotFound() bool
+}
+
+func notFound(message string) NotFound {
+	if message == "" {
+		message = "Not found"
+	}
+	return NotFoundError(message)
+}
+
+type NotFoundError string
+
+func (n NotFoundError) Error() string {
+	return string(n)
+}
+
+func (n NotFoundError) NotFound() bool {
+	return true
+}
+
+func isNotFound(err error) bool {
+	if e, ok := err.(NotFound); ok {
+		return e.NotFound()
+	}
+	return false
+}
+
+type ApiPayload struct {
+	Name  string
+	Value json.RawMessage
+}
+
+var apiPayloadJSONTemplate string = `{"%s": %s}`
+
+func (a *ApiPayload) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(apiPayloadJSONTemplate, a.Name, a.Value)), nil
+}
+
+func (a *ApiPayload) UnmarshalJSON(data []byte) error {
+	var f interface{}
+	err := json.Unmarshal(data, &f)
+	if err != nil {
+		return err
+	}
+	m := f.(map[string]interface{})
+	for k := range m {
+		a.Name = k
+	}
+	val := m[a.Name]
+	raw, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	a.Value = raw
+	return nil
+}
+
+type HttpClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type Api struct {
+	baseUrl *url.URL          // API base URL
+	path    string            // API endpoint path
+	Client  func() HttpClient // HTTP Client to do the requests
+}
+
+func (a *Api) processRequest(method string, path string, body io.Reader) (*http.Response, error) {
+	requestUrl, err := a.baseUrl.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+	request, err := http.NewRequest(method, requestUrl.String(), body)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Accept", "application/json")
+	if err != nil {
+		return nil, err
+	}
+	response, err := a.Client().Do(request)
+	if err != nil {
+		return nil, err
+	}
+	return response, nil
+}
+
+// All populates the data passed in with the results found at the API endpoint
+// data must be a slice of pointers to the resource corresponding with the
+// endpoint
+// params contains additional query parameters and may be nil
+func (a *Api) All(data interface{}, params url.Values) error {
+	completePath := a.path
+	if params != nil {
+		completePath += "?" + params.Encode()
+	}
+	response, err := a.processRequest("GET", completePath, nil)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	var payload []*ApiPayload
+	err = json.Unmarshal(responseBytes, &payload)
+	if err != nil {
+		return err
+	}
+	var rawPayloads []*json.RawMessage
+	for _, p := range payload {
+		rawPayloads = append(rawPayloads, &p.Value)
+	}
+	marshaled, err := json.Marshal(&rawPayloads)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(marshaled, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Find gets the data specified by id
+// id is accepted as primitive data type or as type which implements
+// the fmt.Stringer interface
+func (a *Api) Find(id, data interface{}) error {
+	// TODO: It's nice to build "templates" for Sprintf, but it's not comprehensible
+	findTemplate := fmt.Sprintf("%s/%%%%%%c", a.path)
+	idVerb := 'v'
+	_, ok := id.(fmt.Stringer)
+	if ok {
+		idVerb = 's'
+	}
+	pathTemplate := fmt.Sprintf(findTemplate, idVerb)
+	response, err := a.processRequest("GET", fmt.Sprintf(pathTemplate, id), nil)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode == 404 {
+		return notFound("")
+	}
+	defer response.Body.Close()
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	var payload ApiPayload
+	err = json.Unmarshal(responseBytes, &payload)
+	if err != nil {
+		return err
+	}
+	marshaled, err := json.Marshal(&payload.Value)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(marshaled, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *Api) Create(data interface{}) error {
+	return errors.New("Not implemented yet")
+}
+
+func (a *Api) Update(data interface{}) error {
+	return errors.New("Not implemented yet")
+}
+
+func (a *Api) Delete(data interface{}) error {
+	return errors.New("Not implemented yet")
+}
--- harvest/harvest_test.go
+++ harvest/harvest_test.go
@@ -43,3 +43,326 @@ func createClient(t *testing.T) *Harvest {
 	}
 	return client
 }
+
+type testHttpClient struct {
+	testRequest  *http.Request
+	testResponse *http.Response
+	testError    error
+}
+
+func (t *testHttpClient) Do(request *http.Request) (*http.Response, error) {
+	t.testRequest = request
+	return t.testResponse, t.testError
+}
+
+func TestProcessRequest(t *testing.T) {
+	requestMethod := "GET"
+	bodyContent := []byte("BODY")
+	body := bytes.NewBuffer(bodyContent)
+	path := "foobar"
+	url, _ := url.Parse("http://www.example.com")
+
+	testClient := testHttpClient{}
+
+	client := func() HttpClient {
+		return &testClient
+	}
+
+	api := Api{
+		baseUrl: url,
+		path:    path,
+		Client:  client,
+	}
+
+	// Test
+	_, err := api.processRequest(requestMethod, path, body)
+
+	// Expectations
+	if err != nil {
+		t.Logf("Expected to get no error, got '%v'", err)
+		t.Fail()
+	}
+
+	testRequest := testClient.testRequest
+
+	if testRequest == nil {
+		t.Log("Expected request not to be nil")
+		t.Fail()
+	}
+
+	if testRequest.Method != requestMethod {
+		t.Logf("Expected request to have method '%s', got '%s'", requestMethod, testRequest.Method)
+		t.Fail()
+	}
+
+	requestUrl := testRequest.URL.String()
+	expectedUrl := url.String() + "/" + path
+
+	if requestUrl != expectedUrl {
+		t.Logf("Expected request to have URL '%s', got '%s'", expectedUrl, requestUrl)
+		t.Fail()
+	}
+
+	expectedContentType := "application/json"
+	actualContentType := testRequest.Header.Get("Content-Type")
+
+	if actualContentType != expectedContentType {
+		t.Logf("Expected request to have Content-Type header set to '%s', got '%s'", expectedContentType, actualContentType)
+		t.Fail()
+	}
+
+	expectedAcceptHeader := "application/json"
+	actualAcceptHeader := testRequest.Header.Get("Accept")
+
+	if actualAcceptHeader != expectedAcceptHeader {
+		t.Logf("Expected request to have Accept header set to '%s', got '%s'", expectedAcceptHeader, actualAcceptHeader)
+		t.Fail()
+	}
+
+	actualBodyBytes, err := ioutil.ReadAll(testRequest.Body)
+	if err != nil {
+		t.Logf("Expected to get no error, got '%v'", err)
+		t.Fail()
+	}
+
+	if !bytes.Equal(actualBodyBytes, bodyContent) {
+		t.Logf("Expected request to have body '%s', got '%s'", string(bodyContent), string(actualBodyBytes))
+		t.Fail()
+	}
+}
+
+type testPayload struct {
+	Id   int
+	Data string
+}
+
+func TestApiPayloadMarshalJSON(t *testing.T) {
+	testData := testPayload{
+		Id:   12,
+		Data: "foobar",
+	}
+	testJson, err := json.Marshal(&testData)
+	if err != nil {
+		t.Fail()
+		t.Logf("Got error: %v\n", err)
+	}
+
+	payload := ApiPayload{
+		Name:  "Test",
+		Value: testJson,
+	}
+
+	marshaled, err := json.Marshal(&payload)
+	if err != nil {
+		t.Fail()
+		t.Logf("Expected no error, got: %v", err)
+	}
+
+	expected := `{"Test":{"Id":12,"Data":"foobar"}}`
+
+	if string(marshaled) != expected {
+		t.Fail()
+		t.Logf("Expected marshaled JSON to equal '%s', got '%s'", expected, string(marshaled))
+	}
+
+}
+
+func TestApiPayloadUnmarshalJSON(t *testing.T) {
+	testJson := `{"Test":{"Id":12,"Data":"foobar"}}`
+	var payload ApiPayload
+
+	err := json.Unmarshal([]byte(testJson), &payload)
+
+	if err != nil {
+		t.Fail()
+		t.Logf("Expected no error, got: %v", err)
+	}
+
+	expected := `harvest.ApiPayload{Name:"Test", Value:json.RawMessage}`
+	actual := fmt.Sprintf(`%T{Name:"%s", Value:%T}`, payload, payload.Name, payload.Value)
+
+	if actual != expected {
+		t.Fail()
+		t.Logf("Expected unmarshaled JSON to equal '%s', got '%s'", expected, actual)
+	}
+
+	expectedValue := []byte(`{"Id":12,"Data":"foobar"}`)
+	sort.Sort(sortedBytes(expectedValue))
+	sort.Sort(sortedBytes(payload.Value))
+
+	if !bytes.Equal(expectedValue, payload.Value) {
+		t.Logf("Expected value to equal '%s', got '%s'", string(expectedValue), string(payload.Value))
+		t.Fail()
+	}
+}
+
+func TestApiAll(t *testing.T) {
+	path := "foobar"
+	uri, _ := url.Parse("http://www.example.com")
+
+	testClient := &testHttpClient{}
+
+	client := func() HttpClient {
+		return testClient
+	}
+
+	api := Api{
+		baseUrl: uri,
+		path:    path,
+		Client:  client,
+	}
+
+	testData := testPayload{
+		Id:   12,
+		Data: "foobar",
+	}
+	testJson, err := json.Marshal(&testData)
+	if err != nil {
+		t.Logf("Got error: %v\n", err)
+		t.Fail()
+	}
+	payload := []*ApiPayload{
+		&ApiPayload{
+			Name:  "Test",
+			Value: testJson,
+		},
+	}
+	marshaled, err := json.Marshal(&payload)
+	if err != nil {
+		t.Logf("Expected no error, got: %v", err)
+		t.Fail()
+	}
+
+	testClient.testResponse = &http.Response{
+		Body: ioutil.NopCloser(bytes.NewBuffer(marshaled)),
+	}
+
+	var data []*testPayload
+
+	err = api.All(&data, nil)
+
+	if err != nil {
+		t.Logf("Expected no error, got: %v", err)
+		t.Fail()
+	}
+
+	if len(data) != 1 {
+		t.Logf("Expected one item in data, got: %d", len(data))
+		t.FailNow()
+	}
+
+	if data[0] == nil {
+		t.Logf("Expected first item in data not to be nil")
+		t.FailNow()
+	}
+
+	if !reflect.DeepEqual(*data[0], testData) {
+		t.Logf("Expected data to equal %+#v, got: %+#v", testData, *data[0])
+		t.Fail()
+	}
+
+	// Testing url query params
+	testClient.testResponse = &http.Response{
+		Body: ioutil.NopCloser(bytes.NewBuffer([]byte{})),
+	}
+
+	data = nil
+	params := url.Values{}
+	params.Add("foo", "bar")
+
+	err = api.All(&data, params)
+
+	testRequestUrl := testClient.testRequest.URL
+
+	if !reflect.DeepEqual(testRequestUrl.Query(), params) {
+		t.Logf("Expected query params from request to be '%v', got: '%v'", params, testRequestUrl.Query())
+		t.Fail()
+	}
+
+}
+
+func TestApiFind(t *testing.T) {
+	path := "foobar"
+	uri, _ := url.Parse("http://www.example.com")
+
+	testClient := &testHttpClient{}
+
+	client := func() HttpClient {
+		return testClient
+	}
+
+	api := Api{
+		baseUrl: uri,
+		path:    path,
+		Client:  client,
+	}
+
+	testData := testPayload{
+		Id:   12,
+		Data: "foobar",
+	}
+	testJson, err := json.Marshal(&testData)
+	if err != nil {
+		t.Logf("Got error: %v\n", err)
+		t.Fail()
+	}
+	payload := ApiPayload{
+		Name:  "Test",
+		Value: testJson,
+	}
+	marshaled, err := json.Marshal(&payload)
+	if err != nil {
+		t.Logf("Expected no error, got: %v", err)
+		t.Fail()
+	}
+
+	testClient.testResponse = &http.Response{
+		Body: ioutil.NopCloser(bytes.NewBuffer(marshaled)),
+	}
+
+	var data *testPayload
+
+	err = api.Find(12, &data)
+
+	if err != nil {
+		t.Logf("Expected no error, got: %v", err)
+		t.Fail()
+	}
+
+	if data == nil {
+		t.Logf("Expected to find one item, got nil")
+		t.FailNow()
+	}
+
+	if !reflect.DeepEqual(*data, testData) {
+		t.Logf("Expected data to equal %+#v, got: %+#v", testData, *data)
+		t.Fail()
+	}
+
+	// Testing nonexisting id
+	testClient.testResponse = &http.Response{
+		StatusCode: 404,
+		Body:       ioutil.NopCloser(strings.NewReader("")),
+	}
+
+	data = nil
+
+	err = api.Find(999, &data)
+
+	if err == nil {
+		t.Logf("Expected an error, got: nil")
+		t.Fail()
+	}
+	if err != nil {
+		if !isNotFound(err) {
+			t.Logf("Expected NotFound error, got: %v", err)
+			t.Fail()
+		}
+	}
+}
+
+type sortedBytes []byte
+
+func (s sortedBytes) Len() int           { return len(s) }
+func (s sortedBytes) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s sortedBytes) Less(i, j int) bool { return s[i] < s[j] }
