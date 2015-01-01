c9c8dc95b3e73ae7a48c85319f0baf1374264ee7
Michael Wagner <mitch.wagna@gmail.com>
2015-01-01 19:53:46 +0100

    Fix broken cli methods

--- cmd/harvest/main.go
+++ cmd/harvest/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"code.google.com/p/goauth2/oauth"
 	"github.com/mitch000001/go-harvest/harvest"
 )
@@ -11,20 +13,39 @@ func main() {
 
 // NewBasicAuthClient creates a new Client with BasicAuth as authentication method
 func NewBasicAuthClient(subdomain string, config *BasicAuthConfig) (*harvest.Harvest, error) {
-	h, err := harvest.NewHarvest(subdomain)
+	clientProvider := &Transport{Config: config}
+	h, err := harvest.NewHarvest(subdomain, BuildClientProvider(clientProvider.Client))
 	if err != nil {
 		return nil, err
 	}
-	h.authenticationTransport = &Transport{Config: config}
 	return h, nil
 }
 
 // NewOAuthClient creates a new Client with OAuth as authentication method
 func NewOAuthClient(subdomain string, config *oauth.Config) (*harvest.Harvest, error) {
-	h, err := harvest.NewHarvest(subdomain)
+	clientProvider := &oauth.Transport{Config: config}
+	h, err := harvest.NewHarvest(subdomain, BuildClientProvider(clientProvider.Client))
 	if err != nil {
 		return nil, err
 	}
-	h.authenticationTransport = &oauth.Transport{Config: config}
 	return h, err
+}
+
+type clientFunc func() *http.Client
+
+func (cf clientFunc) Client() harvest.HttpClient {
+	return cf()
+}
+
+type clientProviderWrapper struct {
+	clientFunc clientFunc
+}
+
+func (cw *clientProviderWrapper) Client() harvest.HttpClient {
+	return cw.clientFunc()
+}
+
+func BuildClientProvider(f func() *http.Client) harvest.HttpClientProvider {
+	wrapper := &clientProviderWrapper{clientFunc: f}
+	return wrapper
 }
