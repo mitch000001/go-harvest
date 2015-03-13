d263e64c5388ae3c9885d6169d6befb350497eae
Michael Wagner <mitch.wagna@gmail.com>
2015-03-13 20:13:35 +0100

    Add oauth2 support

--- harvest/auth/auth.go
+++ harvest/auth/auth.go
@@ -1,11 +1,32 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
 
 	"code.google.com/p/goauth2/oauth"
 	"github.com/mitch000001/go-harvest/harvest"
+	"golang.org/x/oauth2"
 )
+
+var oauth2AuthURLTemplate string = "%s/oauth2/authorize"
+var oauth2TokenURLTemplate string = "%s/oauth2/token"
+
+// TODO(mw): Handling for wrong subdomains
+func NewOauth2EndpointForSubdomain(subdomain string) oauth2.Endpoint {
+	return oauth2.Endpoint{
+		AuthURL:  Oauth2AuthURL(subdomain),
+		TokenURL: Oauth2TokenURL(subdomain),
+	}
+}
+
+func Oauth2AuthURL(subdomain string) string {
+	return fmt.Sprintf(oauth2AuthURLTemplate, subdomain)
+}
+
+func Oauth2TokenURL(subdomain string) string {
+	return fmt.Sprintf(oauth2TokenURLTemplate, subdomain)
+}
 
 // NewBasicAuthClient creates a new ClientProvider with BasicAuth as authentication method
 func NewBasicAuthClientProvider(config *BasicAuthConfig) harvest.HttpClientProvider {
