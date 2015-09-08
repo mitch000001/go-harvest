1c94aa9a94ab8bfccf466d5f4a1f6ef33e06d6bf
Michael Wagner <mitch.wagna@gmail.com>
2015-09-08 10:23:49 +0200

    Filter mock dayentries properly

--- harvest/mock/day_entry_service.go
+++ harvest/mock/day_entry_service.go
@@ -8,7 +8,8 @@ import (
 )
 
 type DayEntryService struct {
-	Entries []*harvest.DayEntry
+	Entries       []*harvest.DayEntry
+	BillableTasks []int
 	harvest.CrudEndpoint
 }
 
@@ -17,9 +18,42 @@ func (d DayEntryService) All(data interface{}, params url.Values) error {
 	if err != nil {
 		return fmt.Errorf("Error while parsing timeframe: %v", err)
 	}
+	filter := dayEntryFilter{}
+	timeframeFilter := func(e *harvest.DayEntry) bool {
+		return timeframe.IsInTimeframe(e.SpentAt)
+	}
+	filter.add(timeframeFilter)
+	billableParam := params.Get("billable")
+	if billableParam != "" {
+		var billingFilter func(*harvest.DayEntry) bool
+		if billableParam == "yes" {
+			billingFilter = func(e *harvest.DayEntry) bool {
+				res := false
+				for _, taskId := range d.BillableTasks {
+					if e.TaskId == taskId {
+						res = true
+					}
+				}
+				return res
+			}
+		} else if billableParam == "no" {
+			billingFilter = func(e *harvest.DayEntry) bool {
+				res := true
+				for _, taskId := range d.BillableTasks {
+					if e.TaskId == taskId {
+						res = false
+					}
+				}
+				return res
+			}
+		} else {
+			return fmt.Errorf("Malformed billable param: %s", billableParam)
+		}
+		filter.add(billingFilter)
+	}
 	entries := make([]*harvest.DayEntry, 0)
 	for _, entry := range d.Entries {
-		if timeframe.IsInTimeframe(entry.SpentAt) {
+		if filter.apply(entry) {
 			entries = append(entries, entry)
 		}
 	}
@@ -37,4 +71,18 @@ func (d DayEntryService) URL() url.URL {
 
 func NewDayEntryService(service DayEntryService) *harvest.DayEntryService {
 	return harvest.NewDayEntryService(service)
+}
+
+type dayEntryFilter []func(*harvest.DayEntry) bool
+
+func (d *dayEntryFilter) add(fn func(*harvest.DayEntry) bool) {
+	*d = append(*d, fn)
+}
+
+func (d dayEntryFilter) apply(e *harvest.DayEntry) bool {
+	result := true
+	for _, fn := range d {
+		result = result && fn(e)
+	}
+	return result
 }
--- harvest/mock/day_entry_service_test.go
+++ harvest/mock/day_entry_service_test.go
@@ -40,7 +40,11 @@ func TestDayEntryServiceAll(t *testing.T) {
 		Entries: []*harvest.DayEntry{
 			&harvest.DayEntry{ID: 1, Hours: 8, TaskId: 2, SpentAt: harvest.Date(2015, 1, 1, time.UTC)},
 			&harvest.DayEntry{ID: 2, Hours: 8, TaskId: 2, SpentAt: harvest.Date(2015, 2, 1, time.UTC)},
+			&harvest.DayEntry{ID: 3, Hours: 8, TaskId: 5, SpentAt: harvest.Date(2015, 1, 19, time.UTC)},
+			&harvest.DayEntry{ID: 4, Hours: 8, TaskId: 7, SpentAt: harvest.Date(2015, 1, 20, time.UTC)},
+			&harvest.DayEntry{ID: 5, Hours: 8, TaskId: 9, SpentAt: harvest.Date(2015, 1, 21, time.UTC)},
 		},
+		BillableTasks: []int{2, 5},
 	}
 
 	var entries []*harvest.DayEntry
@@ -57,6 +61,9 @@ func TestDayEntryServiceAll(t *testing.T) {
 	expectedEntries := []*harvest.DayEntry{
 		&harvest.DayEntry{ID: 1, Hours: 8, TaskId: 2, SpentAt: harvest.Date(2015, 1, 1, time.UTC)},
 		&harvest.DayEntry{ID: 2, Hours: 8, TaskId: 2, SpentAt: harvest.Date(2015, 2, 1, time.UTC)},
+		&harvest.DayEntry{ID: 3, Hours: 8, TaskId: 5, SpentAt: harvest.Date(2015, 1, 19, time.UTC)},
+		&harvest.DayEntry{ID: 4, Hours: 8, TaskId: 7, SpentAt: harvest.Date(2015, 1, 20, time.UTC)},
+		&harvest.DayEntry{ID: 5, Hours: 8, TaskId: 9, SpentAt: harvest.Date(2015, 1, 21, time.UTC)},
 	}
 
 	if !reflect.DeepEqual(expectedEntries, entries) {
@@ -77,6 +84,49 @@ func TestDayEntryServiceAll(t *testing.T) {
 
 	expectedEntries = []*harvest.DayEntry{
 		&harvest.DayEntry{ID: 1, Hours: 8, TaskId: 2, SpentAt: harvest.Date(2015, 1, 1, time.UTC)},
+		&harvest.DayEntry{ID: 3, Hours: 8, TaskId: 5, SpentAt: harvest.Date(2015, 1, 19, time.UTC)},
+		&harvest.DayEntry{ID: 4, Hours: 8, TaskId: 7, SpentAt: harvest.Date(2015, 1, 20, time.UTC)},
+		&harvest.DayEntry{ID: 5, Hours: 8, TaskId: 9, SpentAt: harvest.Date(2015, 1, 21, time.UTC)},
+	}
+
+	if !reflect.DeepEqual(expectedEntries, entries) {
+		t.Logf("Expected entries to equal\n%q\n\tgot\n%q\n", expectedEntries, entries)
+		t.Fail()
+	}
+
+	// proper filtering for billable
+	params = harvest.Params{}
+
+	err = service.All(&entries, params.ForTimeframe(timeframe).OnlyBillable(true).Values())
+
+	if err != nil {
+		t.Logf("Expected no error, got %T:%v\n", err, err)
+		t.Fail()
+	}
+
+	expectedEntries = []*harvest.DayEntry{
+		&harvest.DayEntry{ID: 1, Hours: 8, TaskId: 2, SpentAt: harvest.Date(2015, 1, 1, time.UTC)},
+		&harvest.DayEntry{ID: 3, Hours: 8, TaskId: 5, SpentAt: harvest.Date(2015, 1, 19, time.UTC)},
+	}
+
+	if !reflect.DeepEqual(expectedEntries, entries) {
+		t.Logf("Expected entries to equal\n%q\n\tgot\n%q\n", expectedEntries, entries)
+		t.Fail()
+	}
+
+	// proper filtering for nonbillable
+	params = harvest.Params{}
+
+	err = service.All(&entries, params.ForTimeframe(timeframe).OnlyBillable(false).Values())
+
+	if err != nil {
+		t.Logf("Expected no error, got %T:%v\n", err, err)
+		t.Fail()
+	}
+
+	expectedEntries = []*harvest.DayEntry{
+		&harvest.DayEntry{ID: 4, Hours: 8, TaskId: 7, SpentAt: harvest.Date(2015, 1, 20, time.UTC)},
+		&harvest.DayEntry{ID: 5, Hours: 8, TaskId: 9, SpentAt: harvest.Date(2015, 1, 21, time.UTC)},
 	}
 
 	if !reflect.DeepEqual(expectedEntries, entries) {
@@ -105,6 +155,61 @@ func TestDayEntryServiceURL(t *testing.T) {
 
 	if expectedUrl.String() != actualUrl.String() {
 		t.Logf("Expected URL to return\n%q\n\tgot\n%q\n", expectedUrl, actualUrl)
+		t.Fail()
+	}
+}
+
+func TestDayEntryFilterAdd(t *testing.T) {
+	filter := dayEntryFilter{}
+
+	filterOdd := func(e *harvest.DayEntry) bool {
+		return e.ID%2 != 0
+	}
+
+	filter.add(filterOdd)
+
+	if len(filter) != 1 {
+		t.Logf("Expected filter to have one item, got %d\n", len(filter))
+		t.Fail()
+	}
+}
+
+func TestDayEntryFilterApply(t *testing.T) {
+	filter := dayEntryFilter{}
+
+	filterOdd := func(e *harvest.DayEntry) bool {
+		return e.ID%2 != 0
+	}
+	filterMod3 := func(e *harvest.DayEntry) bool {
+		return e.ID%3 != 0
+	}
+
+	filter.add(filterOdd)
+	filter.add(filterMod3)
+
+	dataSet := []*harvest.DayEntry{
+		&harvest.DayEntry{ID: 1},
+		&harvest.DayEntry{ID: 2},
+		&harvest.DayEntry{ID: 3},
+		&harvest.DayEntry{ID: 4},
+		&harvest.DayEntry{ID: 5},
+	}
+
+	expectedDataSet := []*harvest.DayEntry{
+		&harvest.DayEntry{ID: 1},
+		&harvest.DayEntry{ID: 5},
+	}
+
+	var result []*harvest.DayEntry
+
+	for _, entry := range dataSet {
+		if filter.apply(entry) {
+			result = append(result, entry)
+		}
+	}
+
+	if !reflect.DeepEqual(expectedDataSet, result) {
+		t.Logf("Expected result to equal\n%q\n\tgot\n%q\n", expectedDataSet, result)
 		t.Fail()
 	}
 }
